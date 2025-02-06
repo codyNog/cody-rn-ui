74995137f8d92e63faf86fbb84b889abc6e408b1
codynog <yoneyoneyone99@gmail.com>
2025-02-07 02:31:17 +0900

    initial commit

---
+++ plopfile.cjs
@@ -0,0 +1,26 @@
+module.exports = (plop) => {
+    plop.setGenerator("ui", {
+      description: "ui",
+      prompts: [
+        {
+          type: "input",
+          name: "name",
+          message: "ui name please",
+        },
+      ],
+      actions: [
+        {
+          type: "add",
+          path: "src/{{pascalCase name}}/index.tsx",
+          templateFile: "plop-templates/packages/app/ui/index.tsx.hbs",
+        },
+        {
+          type: "add",
+          path: "src/{{pascalCase name}}/index.stories.tsx",
+          templateFile:
+            "plop-templates/packages/app/ui/index.stories.tsx.hbs",
+        },
+      ],
+    });
+  };
+  
