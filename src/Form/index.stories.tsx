81ad4612f925064cfe3844c4a27302def36e6c31
codyNog <yoneyoneyone99@gmail.com>
2025-03-02 02:14:21 +0900

    fix story

--- src/Form/index.stories.tsx
+++ src/Form/index.stories.tsx
@@ -4,10 +4,9 @@ import { useState } from "react";
 import { XStack, YStack } from "tamagui";
 import { Form as Component } from ".";
 import { Button } from "../Button";
-import { Input } from "../Input";
-import { Label } from "../Label";
 import { Select } from "../Select";
 import { getCanvas } from "../libs/storybook";
+import { TextField } from "../TextField";
 
 const meta: Meta<typeof Component> = {
   component: Component,
@@ -25,21 +24,17 @@ export const Default: Story = {
       <Component>
         <YStack gap="$4">
           <XStack gap="$2">
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="type this" />
+            <TextField label="Name" onChange={settType} value={type} />
           </XStack>
           <XStack gap="$2">
-            <Label htmlFor="age">Age</Label>
-            <Input id="age" placeholder="type this" />
+            <TextField label="Age" onChange={settType} value={type} />
           </XStack>
           <XStack gap="$2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="type this" />
+            <TextField label="Email" onChange={settType} value={type} />
           </XStack>
           <XStack gap="$2">
-            <Label htmlFor="type">Type</Label>
             <Select
-              id="type"
+              label="Type"
               value={type}
               options={[
                 { value: "1", label: "Type 1" },
