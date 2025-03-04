6381be42ae5974e136a5556a822b5a8bffe55bd9
codyNog <yoneyoneyone99@gmail.com>
2025-03-05 01:58:32 +0900

    fix AppLayout

--- src/AppLayout/index.tsx
+++ src/AppLayout/index.tsx
@@ -5,19 +5,22 @@ import { Background } from "../Background";
 import { Grid } from "../Grid";
 import { NavigationBar } from "../NavigationBar";
 import type { ComponentProps, ReactNode } from "react";
+import { TopAppBar } from "../TopAppBar";
 
 type Props = {
   children: ReactNode;
+  topAppBar?: ComponentProps<typeof TopAppBar>;
   navigationBar?: ComponentProps<typeof NavigationBar>;
 };
 
-export const AppLayout = ({ children, navigationBar }: Props) => {
+export const AppLayout = ({ children, topAppBar, navigationBar }: Props) => {
   return (
     <Background>
+      {topAppBar && <TopAppBar {...topAppBar} />}
       <SafeAreaView style={{ flex: 1 }}>
         <Grid.Container>{children}</Grid.Container>
-        {navigationBar && <NavigationBar {...navigationBar} />}
       </SafeAreaView>
+      {navigationBar && <NavigationBar {...navigationBar} />}
     </Background>
   );
 };
